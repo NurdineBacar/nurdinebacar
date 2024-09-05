7998e35a1133cd2aaefc4ac334390ddffd678240
NurdineBacar <125458180+NurdineBacar@users.noreply.github.com>
2024-09-06 01:14:57 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -14,5 +14,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: '/nurdinebacar-profile', // Adicione esta linha
+  base: '/nurdinebacar-profile/', // Adicione esta linha
 });
